ed3e05424baec4081203c06b122da6416139dd8a
tpfls123v <tpfls123v@naver.com>
2023-03-14 13:05:01 +0900

    Create project ATM

---
+++ atm/src/atm/AccountManager.java
@@ -0,0 +1,15 @@
+package atm;
+
+import java.util.ArrayList;
+
+public class AccountManager {
+	
+	private static ArrayList<Account> list = new ArrayList<Account>();
+	
+	// Account 메 대한
+	
+	// C reate
+	// R ead
+	// U pdate
+	// D elete
+}
---
+++ atm/src/atm/UserManager.java
@@ -0,0 +1,15 @@
+package atm;
+
+import java.util.ArrayList;
+
+public class UserManager {
+
+	private static ArrayList<User> list = new ArrayList<User>();
+	
+	// User 메 대한
+	
+	// C reate
+	// R ead
+	// U pdate
+	// D elete
+}
